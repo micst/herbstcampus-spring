26e9f8eee6ef6da0ac48549a657e16167086deb7
Michael Stürmer <michael.stuermer@schaeffler.com>
2018-09-04 17:49:56 +0200

    ...

--- part6/src/main/java/hello/Application.java
+++ part6/src/main/java/hello/Application.java
@@ -17,38 +17,38 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner demo(UserRepository repository) {
-//        return (args) -> {
-//            // save a couple of Users
-//            repository.save(new User("Jack", "Bauer"));
-//            repository.save(new User("Chloe", "O'Brian"));
-//            repository.save(new User("Kim", "Bauer"));
-//            repository.save(new User("David", "Palmer"));
-//            repository.save(new User("Michelle", "Dessler"));
-//
-//            // fetch all users
-//            log.info("Users found with findAll():");
-//            log.info("-------------------------------");
-//            for (User user : repository.findAll()) {
-//                log.info(user.toString());
-//            }
-//            log.info("");
-//
-//            // fetch an individual user by ID
-//            User user = repository.findOne(1L);
-//            log.info("User found with findOne(1L):");
-//            log.info("--------------------------------");
-//            log.info(user.toString());
-//            log.info("");
-//
-//            // fetch users by last name
-//            log.info("User found with findByLastName('Bauer'):");
-//            log.info("--------------------------------------------");
-//            for (User bauer : repository.findByLastName("Bauer")) {
-//                log.info(bauer.toString());
-//            }
-//            log.info("");
-//        };
-//    }
+    @Bean
+    public CommandLineRunner demo(UserRepository repository) {
+        return (args) -> {
+            // save a couple of Users
+            repository.save(new User("Jack", "Bauer"));
+            repository.save(new User("Chloe", "O'Brian"));
+            repository.save(new User("Kim", "Bauer"));
+            repository.save(new User("David", "Palmer"));
+            repository.save(new User("Michelle", "Dessler"));
+
+            // fetch all users
+            log.info("Users found with findAll():");
+            log.info("-------------------------------");
+            for (User user : repository.findAll()) {
+                log.info(user.toString());
+            }
+           log.info("");
+
+            // fetch an individual user by ID
+            User user = repository.findOne(1L);
+            log.info("User found with findOne(1L):");
+            log.info("--------------------------------");
+            log.info(user.toString());
+            log.info("");
+
+            // fetch users by last name
+            log.info("User found with findByLastName('Bauer'):");
+            log.info("--------------------------------------------");
+            for (User bauer : repository.findByLastName("Bauer")) {
+                log.info(bauer.toString());
+            }
+            log.info("");
+        };
+    }
 }
